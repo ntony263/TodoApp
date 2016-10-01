eb61a68764f3440aa5683224cba327bb41d3b550
Thinh Nguyen <nht9d2@gmail.com>
2016-10-01 15:14:57 +0700

    Version 7

--- app/src/main/java/com/codepath/simpletodoapp/MainActivity.java
+++ app/src/main/java/com/codepath/simpletodoapp/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
     SQLiteOpenHelper dbhp0;
     SQLiteDatabase db;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
